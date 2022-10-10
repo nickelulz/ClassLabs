924bc4cf4cc93a1ff1b673f6b461ce402b63f11f
nickel <sandmanmufmaf@gmail.com>
2022-10-10 11:54:56 -0500

    Add files via upload

---
+++ labs/Sets/Lab07a.java
@@ -0,0 +1,22 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.*;
+
+public class Lab07a
+{
+	public static void main( String args[] )
+	{
+		String list = "a b c d e f g h a b c d e f g h i j k"; 
+		System.out.println("Original List : " + list);
+		out.println("Uniques : " + UniquesDupes.getUniques(list));
+		out.println("Dupes : " + UniquesDupes.getDupes(list) + "\n\n");
+		
+		//more test cases
+				
+	}
+}
