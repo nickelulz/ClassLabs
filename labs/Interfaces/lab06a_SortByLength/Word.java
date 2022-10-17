a76bec613fa0a568933c0b0df14887fd1256d94e
nickel <sandmanmufmaf@gmail.com>
2022-10-17 12:05:29 -0500

    Add files via upload

---
+++ labs/Interfaces/lab06a_SortByLength/Word.java
@@ -0,0 +1,16 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Word implements Comparable<Word>
+{
+	//add an instance variable and a constructor
+
+	//add a compareTo
+
+	//add a toString
+}
---
+++ labs/Interfaces/lab06e_Gradebook/Student.java
@@ -0,0 +1,97 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Arrays;
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.util.Arrays.*;
+
+public class Student implements Comparable<Student>
+{
+	private String myName;
+	private Grades myGrades;
+	
+	public Student()
+	{
+		setName("");
+		setGrades("");
+	}
+	
+	public Student(String name, String gradeList)
+	{
+
+
+
+	}
+	
+	public void setName(String name)
+	{
+
+
+	}	
+	
+	public void setGrades(String gradeList)
+	{
+
+	
+	}
+	
+	public void setGrade(int spot, double grade)
+	{
+
+
+	}
+
+	public String getName()
+	{
+		return "";
+	}
+	
+	public int getNumGrades()
+	{
+		return 0;
+	}
+
+	public double getSum()
+	{
+		return 0.0;
+	}
+	
+	public double getAverage()
+	{
+		return 0.0;
+	}
+
+	public double getAverageMinusLow()
+	{
+		return 0.0;
+	}
+	
+	public double getHighGrade()
+	{
+		return 0.0;		
+	}
+	
+	public double getLowGrade()
+	{
+		return 0.0;	
+	}
+
+	public int compareTo(Student param)
+	{
+		return 0;
+	}
+	
+	public boolean equals(Object obj)
+	{
+		return false;
+	}
+	
+	public String toString()
+	{
+		return "";
+	}	
+}
